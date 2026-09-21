3d593409e6910a57742f30e339103284cd4a809d
yuviverma04 <yuviverma04@users.noreply.github.com>
2026-09-21 15:39:57 +0530

    fix gmail ipv4 smtp connection

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,6 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -8,11 +11,16 @@ const nodemailer = require("nodemailer");
 // =====================================================
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
   family: 4,
 });
 
