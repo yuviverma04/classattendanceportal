d755706a66b91380fba9ed6128f875ab198d1e61
yuviverma04 <yuviverma04@users.noreply.github.com>
2026-09-21 15:32:32 +0530

    fix gmail email connection

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,6 +13,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  family: 4,
 });
 
 // =====================================================
